4ff642c648e00351efda1e407d9bd63f1be6569c
KatherineEbel <kathyebel@me.com>
2016-10-24 14:07:49 -0400

    Add interface elements to storyboard

---
+++ BoutTime/Event.swift
@@ -0,0 +1,16 @@
+//
+//  Event.swift
+//  BoutTime
+//
+//  Created by Katherine Ebel on 10/23/16.
+//  Copyright © 2016 Katherine Ebel. All rights reserved.
+//
+
+import Foundation
+
+protocol EventType {
+  var name: String { get }
+  var date: NSDate { get }
+}
+
+
