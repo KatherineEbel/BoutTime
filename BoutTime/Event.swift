2be860b7aeff8d552798912f905f9b832050ebe8
KatherineEbel <kathyebel@me.com>
2016-10-25 12:52:29 -0400

    Add plist for Events

--- BoutTime/Event.swift
+++ BoutTime/Event.swift
@@ -2,7 +2,7 @@
 //  Event.swift
 //  BoutTime
 //
-//  Created by Katherine Ebel on 10/23/16.
+//  Created by Katherine Ebel on 10/25/16.
 //  Copyright © 2016 Katherine Ebel. All rights reserved.
 //
 
@@ -12,5 +12,43 @@ protocol EventType {
   var name: String { get }
   var date: NSDate { get }
 }
+enum ResourceError: Error {
+  case InvalidResource
+  case ConversionError
+}
 
+struct Event: EventType {
+  let name: String
+  let date: NSDate
+}
 
+class PlistConverter {
+  class func dictionaryFromFile(resource: String, ofType type: String) throws -> [String: AnyObject] {
+    guard let path = Bundle.main.path(forResource: resource, ofType: type) else {
+      throw ResourceError.InvalidResource
+    }
+    
+    guard let dictionary = NSDictionary.init(contentsOfFile: path),
+    let castDictionary = dictionary as? [String: AnyObject] else {
+      throw ResourceError.ConversionError
+    }
+    return castDictionary
+  }
+}
+
+class EventUnarchiver {
+  class func vendingInventoryFromDictionary(dictionary: [String: AnyObject]) throws -> [EventType] {
+    var events: [EventType]
+    for (key, value) in dictionary {
+      if let eventDict = value as? [String: Double],
+        let price = itemDict["price"], let quantity = itemDict["quantity"] {
+        let item = VendingItem(price: price, quantity: quantity)
+        guard let vendingKey = VendingSelection.init(rawValue: key) else {
+          throw InventoryError.InvalidKey
+        }
+        inventory.updateValue(item, forKey: vendingKey)
+      }
+    }
+    return inventory
+  }
+}
